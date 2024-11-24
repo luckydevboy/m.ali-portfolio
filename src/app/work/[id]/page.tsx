76c3f524cf08ef3be61c35e8fe4edfa7ffa43f2c
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-11-24 14:43:13 +0330

    style: handle missed items related to the font and color according to the design

--- src/app/work/[id]/page.tsx
+++ src/app/work/[id]/page.tsx
@@ -12,18 +12,18 @@ const Work = () => {
   const work = data?.data;
 
   return (
-    <div className="my-16">
-      <h1 className="mb-6 font-poppins text-center text-primary text-5xl">
+    <div className="mt-8 lg:mt-16">
+      <h1 className="mb-6 font-poppins text-center text-primary text-2xl lg:text-5xl">
         {work?.title}
       </h1>
-      <div className="max-w-[800px] mx-auto mb-12 px-5 font-nunito-sans text-center text-tertiary text-lg">
+      <div className="max-w-[800px] mx-auto mb-12 px-5 font-nunito-sans text-center text-tertiary text-sm lg:text-lg">
         {work && <BlocksRenderer content={work.description} />}
       </div>
-      <div className="space-y-8">
+      <div className="space-y-4 lg:space-y-8">
         {work?.images.map((image) => (
           <div
             key={image.documentID}
-            className="relative w-full h-[calc(50vw)]"
+            className="relative w-full h-[calc(100vw)] lg:h-[calc(50vw)]"
           >
             <Image
               src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${image.url}`}
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -17,22 +17,24 @@ const Header = ({ className }: Props) => {
 
   return (
     <header className={`relative py-7 px-5 ${className}`}>
-      <h1 className="text-lg sm:text-2xl font-bold text-center font-poppins">
+      <h1 className="text-lg sm:text-2xl font-semibold text-primary text-center font-poppins">
         Mohammad Ali Ghasemi
       </h1>
       <MenuIcon
-        className="h-6 w-6 absolute right-5 top-1/2 -translate-y-1/2"
+        className="h-6 w-6 absolute right-5 top-1/2 -translate-y-1/2 text-primary"
         onClick={() => setMenuIsOpen(!menuIsOpen)}
       />
 
       {menuIsOpen && (
         <nav className="fixed inset-0 z-10 bg-white">
           <XIcon
-            className="absolute right-5 top-7"
+            className="absolute right-5 top-7 text-primary"
             onClick={() => setMenuIsOpen(false)}
           />
-          <h2 className="text-center mt-12 text-2xl mb-8 font-poppins">Work</h2>
-          <ul className="flex flex-col gap-y-6 text-center font-nunito-sans">
+          <h2 className="text-center mt-12 text-2xl mb-8 font-poppins text-tertiary font-bold">
+            {"Work".toUpperCase()}
+          </h2>
+          <ul className="flex flex-col gap-y-8 text-center font-nunito-sans text-tertiary text-2xl">
             {works?.map((work) => (
               <li key={work.documentId}>
                 <Link
